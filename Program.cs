e3ba71198c62afeb3870d4087e210f660f656fec
mjbelanic <mjbelanic@aol.com>
2017-11-15 18:45:31 -0800

    Data Structure Re-upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack myStack = new Stack();
+            Console.WriteLine(myStack.IsEmpty());
+            Random random = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                int value = random.Next(1, 100);
+                myStack.Push(value);
+                Console.Write(value + " ");
+            }
+            Console.Write("\n");
+            int myStackLength = myStack.Length();
+            Console.WriteLine(myStack.Peek());
+            for(int i = 0; i < myStackLength; i++)
+            {
+                Console.Write(myStack.Pop() + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,56 @@
+﻿namespace DataStructures
+{
+    public class Stack
+    {
+        public Stack() {}
+
+        public class Node
+        {
+            public int data;
+            public Node next;
+
+            public Node(int data)
+            {
+                this.data = data;
+            }
+        };
+
+        private Node top;
+
+        public bool IsEmpty()
+        {
+            return (top == null);
+        }
+
+        public int Peek()
+        {
+            return top.data;
+        }
+        
+        public void Push(int data) {
+            Node node = new Node(data);
+            node.next = top;
+            top = node;
+        }
+
+        public int Pop() {
+            int data = top.data;
+            top = top.next;
+            return data;
+        }
+
+        public int Length()
+        {
+            Node iterator = top;
+            int count = 0;
+            while(iterator != null)
+            {
+                count++;
+                iterator = iterator.next;
+            }
+            return count;
+        }
+    }
+
+
+}
