263db5765b7b7ca554837292b3bd8b774207f446
mjbelanic <mjbelanic@aol.com>
2017-11-17 13:37:03 -0800

    Queue Example Added

--- Program.cs
+++ Program.cs
@@ -7,12 +7,54 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+
+            Console.WriteLine("Linked List Example");
+            Console.WriteLine("===================");
+            LinkedList myList = new LinkedList();
+            myList.PrintList();
+            int removedValue = random.Next(1, 11);
+            myList.Append(random.Next(11, 21));
+            myList.Append(random.Next(21, 31));
+            myList.Append(removedValue);
+            myList.Append(random.Next(31, 41));
+            myList.Append(random.Next(41, 51));
+            myList.PrintList();
+            Console.WriteLine(myList.ListEmpty());
+            myList.Prepend(0);
+            myList.PrintList();
+            myList.DeleteWithValue(removedValue);
+            myList.PrintList();
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Queue Example");
+            Console.WriteLine("=============");
+            Queue myQueue = new Queue();
+            Console.WriteLine(myQueue.IsEmpty());
+            for (int i = 0; i < 10; i++)
+            {
+                int value = random.Next(1, 101);
+                Console.Write(value + " ");
+                myQueue.Add(value);
+            }
+            Console.WriteLine("");
+            Console.WriteLine(myQueue.Peek());
+            for(int i = 0; i < 10; i++)
+            {
+                int topValue = myQueue.Remove();
+                Console.Write(topValue + " ");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+
             Console.WriteLine("Stack Example");
             Console.WriteLine("=============");
 
             Stack myStack = new Stack();
             Console.WriteLine(myStack.IsEmpty());
-            Random random = new Random();
+            
             for (int i = 0; i < 5; i++)
             {
                 int value = random.Next(1, 100);
@@ -29,21 +71,7 @@ namespace DataStructures
 
             Console.WriteLine("");
 
-            Console.WriteLine("Linked List Example");
-            Console.WriteLine("===================");
-            LinkedList myList = new LinkedList();
-            myList.PrintList();
-            myList.Append(1);
-            myList.Append(2);
-            myList.Append(3);
-            myList.Append(4);
-            myList.Append(5);
-            myList.PrintList();
-            Console.WriteLine(myList.ListEmpty());
-            myList.Prepend(0);
-            myList.PrintList();
-            myList.DeleteWithValue(3);
-            myList.PrintList();
+ 
 
 
             Console.ReadLine();
--- Queue.cs
+++ Queue.cs
@@ -6,5 +6,56 @@ namespace DataStructures
 {
     class Queue
     {
+        public Queue() { }
+
+        public class Node{
+            public int data;
+            public Node next;
+
+            public Node(int data)
+            {
+                this.data = data;
+            }            
+        }
+
+        private Node head; //remove from head
+        private Node tail; // add to tail
+
+        public bool IsEmpty()
+        {
+            return (head == null);
+        }
+
+        public int Peek()
+        {
+            return head.data;
+        }
+
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+            if(tail != null)
+            {
+                tail.next = newNode;
+            }
+            tail = newNode;
+            if(head == null)
+            {
+                head = newNode;
+            }
+        }
+
+        public int Remove()
+        {
+            int data = head.data;
+            head = head.next;
+            if(head == null)
+            {
+                tail = null;
+            }
+            return data;
+        }
+
+
     }
 }
