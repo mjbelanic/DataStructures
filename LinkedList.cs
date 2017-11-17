2499595ee5c28a163724ad06bba19426fd665633
mjbelanic <mjbelanic@aol.com>
2017-11-17 11:15:06 -0800

    Linked List Implemented

--- LinkedList.cs
+++ LinkedList.cs
@@ -4,7 +4,94 @@ using System.Text;
 
 namespace DataStructures
 {
-    class LinkedList
+    public class Node
     {
+        public int data;
+        public Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    };
+
+    public class LinkedList
+    {
+        public LinkedList() { }
+
+        private Node head;
+
+        public bool ListEmpty()
+        {
+            return (head == null);
+        }
+
+        public void Append(int data)
+        {
+            if (head == null)
+            {
+                head = new Node(data);
+            }
+            else
+            {
+                Node iterator = head;
+                while (iterator.next != null)
+                {
+                    iterator = iterator.next;
+                }
+                iterator.next = new Node(data);
+            }
+        }
+
+        public void Prepend(int data)
+        {
+            Node newHead = new Node(data);
+            newHead.next = head;
+            head = newHead;
+        }
+
+        public void DeleteWithValue(int data)
+        {
+            if(head == null)
+            {
+                return;
+            }
+            if(head.data == data)
+            {
+                head = head.next;
+                return;
+            }
+            Node iterator = head;
+            while(iterator.next != null)
+            {
+                if (iterator.next.data == data)
+                {
+                    iterator.next = iterator.next.next;
+                    return;
+                }
+                iterator = iterator.next;
+            }
+        }
+
+        public void PrintList()
+        {
+            if (head == null)
+            {
+                Console.Write("List is empty");
+            }
+            else
+            {
+                Node iterator = head;
+                while (iterator != null)
+                {
+                    Console.Write(iterator.data + " ");
+                    iterator = iterator.next;
+                }
+            }
+            Console.WriteLine("");
+        }
+
     }
+
+
 }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Stack Example");
+            Console.WriteLine("=============");
+
             Stack myStack = new Stack();
             Console.WriteLine(myStack.IsEmpty());
             Random random = new Random();
@@ -23,6 +26,26 @@ namespace DataStructures
             {
                 Console.Write(myStack.Pop() + " ");
             }
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Linked List Example");
+            Console.WriteLine("===================");
+            LinkedList myList = new LinkedList();
+            myList.PrintList();
+            myList.Append(1);
+            myList.Append(2);
+            myList.Append(3);
+            myList.Append(4);
+            myList.Append(5);
+            myList.PrintList();
+            Console.WriteLine(myList.ListEmpty());
+            myList.Prepend(0);
+            myList.PrintList();
+            myList.DeleteWithValue(3);
+            myList.PrintList();
+
+
             Console.ReadLine();
         }
     }
